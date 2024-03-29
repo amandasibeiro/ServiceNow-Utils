00acab9ff54d2516c154d89eaff8c40e31a11d63
Amanda Ribeiro <88240895+amandasibeiro@users.noreply.github.com>
2024-03-28 22:07:53 -0300

    Update SetMandatoryOrReadOnly.js

--- Catalog Client Scripts & Client Scripts/SetMandatoryOrReadOnly.js
+++ Catalog Client Scripts & Client Scripts/SetMandatoryOrReadOnly.js
@@ -6,12 +6,12 @@ function onLoad() {
     if ((state == 2 || state == -2 || state == -3 || state == 3 || state == 4) && (data_negociada == '')) {
 
         if (g_user.userID == g_form.getValue('assigned_to')) {
-            g_form.setMandatory('u_data_entrega_negociada', true);
-            g_form.setReadOnly('u_data_entrega_negociada', false);
+            g_form.setMandatory('u_data_entrega', true);
+            g_form.setReadOnly('u_data_entrega', false);
             g_form.addInfoMessage('Preencher a Data de entrega.');
         } else {
-            g_form.setMandatory('u_data_entrega_negociada', false);
-            g_form.setReadOnly('u_data_entrega_negociada', false);
+            g_form.setMandatory('u_data_entrega', false);
+            g_form.setReadOnly('u_data_entrega', false);
             g_form.addInfoMessage('Solicitar ao coordenador para preencher a data de entrega!');
         }
     }
